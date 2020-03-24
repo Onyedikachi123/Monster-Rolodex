9f6755a72008ef3cc426bcd7549a35fc59c93ff9
Kachi123 <kachi.erugo@codex.com.ng>
2020-03-24 16:25:11 +0100

    Updates

---
+++ precache-manifest.f8fa808456d2843dff95c4cd8d105157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b18891e0791d995f9e81ee608e91029",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "e581ed87ac5a26391edb",
+    "url": "/Monster-Rolodex/static/css/main.dbd87680.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/Monster-Rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monster-Rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e581ed87ac5a26391edb",
+    "url": "/Monster-Rolodex/static/js/main.e3bb7d97.chunk.js"
+  },
+  {
+    "revision": "0b4a49a8094b68fdb4f4",
+    "url": "/Monster-Rolodex/static/js/runtime-main.0645fd6d.js"
+  }
+]);
